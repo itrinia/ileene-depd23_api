ca8b4de244cf2148838a7846155734dddfbdaae0
itrinia <isantoso@student.ciputra.ac.id>
2023-12-10 22:22:05 +0700

    update yg oren

--- lib/views/pages/pages.dart
+++ lib/views/pages/pages.dart
@@ -3,7 +3,7 @@ import 'package:depd23_api/services/services.dart';
 import 'package:depd23_api/views/widgets/uiloading.dart';
 import 'package:depd23_api/views/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:depd23_api/views/widgets/uitoast.dart';
+// import 'package:depd23_api/views/widgets/uitoast.dart';
 import 'package:lazy_loading_list/lazy_loading_list.dart';
 
 part 'home.dart';
--- lib/views/widgets/widgets.dart
+++ lib/views/widgets/widgets.dart
@@ -1,7 +1,7 @@
 import 'package:depd23_api/models/models.dart';
 import 'package:depd23_api/shared/shared.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:depd23_api/views/pages/pages.dart';
+// import 'package:flutter_spinkit/flutter_spinkit.dart';
+// import 'package:depd23_api/views/pages/pages.dart';
 
 part 'cardongkir.dart';
